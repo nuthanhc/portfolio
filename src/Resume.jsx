49eff6d880fd018ffe9b8935dc502b0e25a4d61e
nuthanhc <nuthanhc07@gmail.com>
2023-08-18 13:09:18 +0530

    resume conflict solved

---
+++ src/Resume.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import resumeimg from './images/resume.jpg';
+import './Main.css';
+
+export default function MaxWidthDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSave = () => {
+    // Create a temporary link element to trigger download
+    const link = document.createElement('a');
+    link.href = resumeimg;
+    link.download = 'resume.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        RESUME
+      </Button>
+      <Dialog
+        fullWidth='md' // 'lg' is not a valid value, use true or false
+        maxWidth='md' // Disable the maximum width limit
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogContent className="dialog-content">
+          <img src={resumeimg} alt="" />
+        </DialogContent>
+        <DialogActions>
+          <button className="dialog-close-button" onClick={handleSave}>Download</button>
+          <button className="dialog-close-button" onClick={handleClose}>Close</button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
