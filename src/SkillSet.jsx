99a99f1d7a0ee7d8979a15787b1b0b6edd528995
nuthanhc <nuthanhc07@gmail.com>
2023-08-20 22:12:59 +0530

    change web project card design

--- src/SkillSet.jsx
+++ src/SkillSet.jsx
@@ -19,49 +19,63 @@ function SkillSet() {
           exploring limitless horizons, creating captivating digital
           landscapes."
         </p>
-        <a href="http://getpets.000.pe/">
-          <div className="project-card">
-            <div className="card-content">
-              <div className="card-details">
-                <div className="card-image">
+        <div className="card-holder">
+          <h5 className="card-holder-heading text-center">Projects</h5>
+          <div className="card-container">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-img">
                   <img src={petlogo} alt="Project" />
                 </div>
-                <div className="content-text">
-                  <h3 className="text-title">Pet Shop Management</h3>
-                  <p className="text-para">
-                    "An online pet-shop meets various pet requirements through
-                    e-commerce operations."
+                <div className="content">
+                  <h3 className="card-title text-center">
+                    Pet Shop Management
+                  </h3>
+                  <p className="card-info">
+                    In our 5th-semester DBMS project, Team of 2 members crafted
+                    a pet shop management system. This e-commerce platform
+                    enabled pet buying and selling, featuring authentication,
+                    pet listings, and carts. Our cohesive effort highlighted
+                    database, web development skills, and teamwork.
                   </p>
                 </div>
               </div>
-              <div className="card-arrow">
-                <i class="bi bi-caret-right-fill"></i>
+              <div className="card-links">
+                <a href="https://github.com/nuthanhc/getpets">
+                  <i className="bi bi-github"></i>
+                </a>
+                <a href="https://getpets.000.pe/">
+                  <i class="bi bi-link"></i>
+                </a>
               </div>
             </div>
-          </div>
-        </a>
-
-        <a href="https://doarclab.com/">
-          <div className="project-card">
-            <div className="card-content">
-              <div className="card-details">
-                <div className="card-image">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-img">
                   <img src={servicelogo} alt="Project" />
                 </div>
-                <div className="content-text">
-                  <h3 className="text-title">Home Service Management</h3>
-                  <p className="text-para">
-                    "This project simplifies your daily needs by providing a
-                    variety of at-home services."
+                <div className="content">
+                  <h3 className="card-title text-center">
+                    Home Service Management
+                  </h3>
+                  <p className="card-info">
+                    At IRP Innovative Solutions, our intern team of three
+                    swiftly developed a home service management platform using
+                    existing APIs. In just a month, we created a versatile
+                    platform that offers various services - from plumbing to
+                    repairs. This project showcased our efficient teamwork and
+                    Web Development skills.
                   </p>
                 </div>
               </div>
-              <div className="card-arrow">
-                <i class="bi bi-caret-right-fill"></i>
+              <div className="card-links">
+                <a href="https://doarclab.com/">
+                  <i class="bi bi-link"></i>
+                </a>
               </div>
             </div>
           </div>
-        </a>
+        </div>
       </div>
       <div className="web pt-5">
         <h3 className="title text-center">UI / UX Design</h3>
